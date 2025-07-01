71b934eaa02660532e668d5670016fe017670736
Christofer Padilla <christoferpadilla@gmail.com>
2025-06-30 22:52:53 -0400

    Added script to buy hacknet nodes

--- src/TODO.md
+++ src/TODO.md
@@ -1,7 +1,8 @@
 # TODO
 
-* Write a script to upgrade hacknet nodes
 * Write a script for making stock market purchases
+* Fix script-startup.js to not attempt to hack nodes that work succeed
 
 # Done
 * ~~Write a script to purchase private servers~~
+* ~~Write a script to upgrade hacknet nodes~~
---
+++ src/buy-hacknodes.ts
@@ -0,0 +1,90 @@
+/// <reference path="../NetscriptDefinitions.d.ts" />
+import { NS } from "@ns";
+
+enum Upgrade {
+    Level = 1,
+    RAM,
+    Cores
+}
+
+class CheapestUpgrade {
+    index: number;
+    cost: number;
+    type: Upgrade;
+    constructor() {
+        this.index = -1;
+        this.cost = Number.MAX_VALUE;
+    };
+}
+
+export async function main(ns: NS) {
+
+    // Get the number of nodes we own
+    let nodes = ns.hacknet.numNodes();
+
+    while (true) {
+
+        let cheapestUpgrade = new CheapestUpgrade();
+        // Check each node and save whichever upgrade is cheapest
+        for (let i=0; i<nodes; i++) {
+            let upgradeLevelCost = ns.hacknet.getLevelUpgradeCost(i);
+            let nodeStats = ns.hacknet.getNodeStats(i);
+            if (nodeStats.level < 200 && upgradeLevelCost < cheapestUpgrade.cost) {
+                cheapestUpgrade.index = i;
+                cheapestUpgrade.cost = upgradeLevelCost;
+                cheapestUpgrade.type = Upgrade.Level;
+            }
+            let upgradeRAMCost = ns.hacknet.getRamUpgradeCost(i);
+            if (nodeStats.level < 64 && upgradeRAMCost < cheapestUpgrade.cost) {
+                cheapestUpgrade.index = i;
+                cheapestUpgrade.cost = upgradeRAMCost;
+                cheapestUpgrade.type = Upgrade.Level;
+            }
+            let upgradeCoresCost = ns.hacknet.getCoreUpgradeCost(i);
+            if (nodeStats.cores < 16 && upgradeCoresCost < cheapestUpgrade.cost) {
+                cheapestUpgrade.index = i;
+                cheapestUpgrade.cost = upgradeCoresCost;
+                cheapestUpgrade.type = Upgrade.Level;
+            }
+        }
+
+        // Get the cost of purchasing a new node
+        let purchaseNodeCost = ns.hacknet.getPurchaseNodeCost();
+
+        // If there are no nodes, or purchasing a node is cheaper than upgrading,
+        // buy a new node
+        if (nodes == 0 || purchaseNodeCost < cheapestUpgrade.cost) {
+            if (purchaseNodeCost <= ns.getServerMoneyAvailable("home") / 2) {
+                // ns.tprint("Purchasing hacknet node.");
+                ns.hacknet.purchaseNode();
+                nodes++;
+            }
+        } else {
+            // Otherwise, upgrade
+            // Only buy if we have more than double the money of the upgrade
+            if (cheapestUpgrade.cost <= ns.getServerMoneyAvailable("home") / 2) {
+                switch (cheapestUpgrade.type) {
+                    case (Upgrade.Level): {
+                        // ns.tprint(`Upgrading hacknet node ${cheapestUpgrade.index} Level.`);
+                        ns.hacknet.upgradeLevel(cheapestUpgrade.index);
+                        break;
+                    }
+                    case (Upgrade.RAM): {
+                        // ns.tprint(`Upgrading hacknet node ${cheapestUpgrade.index} RAM.`);
+                        ns.hacknet.upgradeRam(cheapestUpgrade.index);
+                        break;
+                    }
+                    case (Upgrade.Cores): {
+                        // ns.tprint(`Upgrading hacknet node ${cheapestUpgrade.index} Core.`);
+                        ns.hacknet.upgradeCore(cheapestUpgrade.index);
+                        break;
+                    }
+                }
+            }
+        }
+
+        //Make the script wait for a second before looping again.
+        //Removing this line will cause an infinite loop and crash the game.
+        await ns.sleep(200);
+    }
+}
